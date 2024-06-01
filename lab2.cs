f98d398f61f5e86a7a5691889fcd99af42727ffb
Erik-Certain <167526282+Erik-Certain@users.noreply.github.com>
2024-06-01 03:06:04 +0000

    .

--- lab2.cs
+++ lab2.cs
@@ -55,44 +55,42 @@ class Program
 {
     static void Main()
     {
-        var clothesList = new ClothesList
-        {
-            new Trousers(100, "S", "Шёлк"),
-            new Shirt(50, "S", 60),
-            new Trousers(200, "M", "Шерсть"),
-            new Shirt(150, "M", 70),
-            new Trousers(300, "S", "Полиэстер")
-        };
-      
-        var clothesSizeS = clothesList.Where(c => c.Size == "S").ToList();
+      var clothesList = new ClothesList
+      {
+          new Trousers(100, "S", "Шёлк"),
+          new Shirt(50, "S", 60),
+          new Trousers(200, "M", "Шерсть"),
+          new Shirt(150, "M", 70),
+          new Trousers(300, "S", "Полиэстер")
+      };
 
-        Console.WriteLine("Введите новые цены для товаров размера S (введите 'end' для завершения ввода):");
+      var clothesSizeS = clothesList.Where(c => c.Size == "S").ToList();
 
-        for (int i = 0; i < clothesSizeS.Count;)
-        {
-            Console.WriteLine($"Введите цену для {clothesSizeS[i].GetType().Name} (текущая цена {clothesSizeS[i].Price}):");
-            string input = Console.ReadLine();
+      Console.WriteLine("Введите новые цены для товаров размера S:");
 
-            if(input == "end")
-            {
-                break;
-            }
+      foreach (var item in clothesSizeS)
+      {
+          Console.WriteLine($"Введите цену для {item.GetType().Name} (текущая цена {item.Price}):");
+          while (true)
+          {
+              string input = Console.ReadLine();
 
-            if (!decimal.TryParse(input, out decimal newPrice) || newPrice <= 0)
-            {
-                Console.WriteLine("Ошибка: введите положительное число больше нуля.");
-                continue;
-            }
+              if (!decimal.TryParse(input, out decimal newPrice) || newPrice <= 0)
+              {
+                  Console.WriteLine("Ошибка: введите положительное число больше нуля.");
+                  continue;
+              }
 
-            clothesSizeS[i].Price = newPrice;
-            i++;
-        }
+              item.Price = newPrice;
+              break;
+          }
+      }
 
-        Console.WriteLine("Обновленный список одежды:");
-        foreach (var item in clothesList)
-        {
-            Console.WriteLine($"Тип: {item.GetType().Name}, Размер: {item.Size}, Цена: {item.Price}");
-        }
+      Console.WriteLine("Обновленный список одежды:");
+      foreach (var item in clothesList)
+      {
+          Console.WriteLine($"Тип: {item.GetType().Name}, Размер: {item.Size}, Цена: {item.Price}");
+      }
 
         Console.WriteLine($"Суммарная стоимость всех вещей размера S: {clothesList.TotalCostForSizeS()}");
         Console.WriteLine($"Средняя стоимость рубашек: {clothesList.AverageShirtPrice()}");
